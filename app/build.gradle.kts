2aae8c7da6e2a26efaecabc064f63474e2d387ed
Simran1993 <harsimranjits5102@gmail.com>
2024-04-05 21:40:39 -0400

    Error Solved

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,9 +35,7 @@ dependencies {
     val room_version = ("2.6.1")
     implementation ("androidx.room:room-runtime:$room_version")
     annotationProcessor ("androidx.room:room-compiler:$room_version")
-
     implementation("androidx.mediarouter:mediarouter:1.7.0")
-    val room_version = "2.6.1"
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
---
+++ app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
@@ -0,0 +1,62 @@
+package algonquin.cst2335.finalproject;
+
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import java.util.Dictionary;
+
+import algonquin.cst2335.finalproject.SongMainActivity;
+import algonquin.cst2335.finalproject.SRSSSunriseMainActivity;
+import algonquin.cst2335.finalproject.Dist.DictionaryMainActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_more_Song) {
+            Intent intent = new Intent(this,SongMainActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        if (id == R.id.action_more_Dict) {
+            Intent intent = new Intent(this,DictionaryMainActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        if (id == R.id.action_more_Sun) {
+            Intent intent = new Intent(this, SRSSSunriseMainActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+}
