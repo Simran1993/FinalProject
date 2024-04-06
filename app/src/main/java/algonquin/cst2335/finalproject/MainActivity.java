ba7c8296bc3631c9a4917656dc0c12d45404280d
Simran1993 <harsimranjits5102@gmail.com>
2024-04-05 21:45:09 -0400

    add Toolbar

--- app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_menu, menu);
+        getMenuInflater().inflate(R.menu.my_menu, menu);
         return true;
     }
 
